b42bdbbe59b962a116d97cb8a13ce7840cc6052c
rjtj2007 <teacherman1@gmail.com>
2017-12-14 19:44:33 -0800

    working through if/else and json on article.js file

--- ryan-ariel/scripts/article.js
+++ ryan-ariel/scripts/article.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function Article (rawDataObj) {
+function Article(rawDataObj) {
   this.author = rawDataObj.author;
   this.authorUrl = rawDataObj.authorUrl;
   this.title = rawDataObj.title;
@@ -9,19 +9,19 @@ function Article (rawDataObj) {
   this.publishedOn = rawDataObj.publishedOn;
 }
 
-// REVIEW: Instead of a global `articles = []` array, let's attach this list of all articles directly to the constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves objects, which means we can add properties/values to them at any time. In this case, the array relates to ALL of the Article objects, so it does not belong on the prototype, as that would only be relevant to a single instantiated Article.
+// REVIEWED: Instead of a global `articles = []` array, let's attach this list of all articles directly to the constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves objects, which means we can add properties/values to them at any time. In this case, the array relates to ALL of the Article objects, so it does not belong on the prototype, as that would only be relevant to a single instantiated Article.
 Article.all = [];
 
-// COMMENT: Why isn't this method written as an arrow function?
-// PUT YOUR RESPONSE HERE
-Article.prototype.toHtml = function() {
+// COMMENTED: Why isn't this method written as an arrow function?
+// This funtion uses a contextual this and so cannot be an arrow function.
+Article.prototype.toHtml = function () {
   let template = Handlebars.compile($('#article-template').text());
 
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
 
-  // COMMENT: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
+  // COMMENTED: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
   // Not sure? Check the docs!
-  // PUT YOUR RESPONSE HERE
+  // The line below is checking to see if this.publishedOn has a value. The question mark and colon in the secion is a ternary operator. This logic is also used in an if/if else/else conditional and simplified here.
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
   this.body = marked(this.body);
 
@@ -32,15 +32,15 @@ Article.prototype.toHtml = function() {
 
 // REVIEW: This function will take the rawData, how ever it is provided, and use it to instantiate all the articles. This code is moved from elsewhere, and encapsulated in a simply-named function for clarity.
 
-// COMMENT: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
-// PUT YOUR RESPONSE HERE
+// COMMENTED: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
+// This function is called just below inside of the if statement of the Article.fetchAll function. rawData represents the parameter of the function. rawData used to be an array of article object values (key/value pairs) in previous labs.
 Article.loadAll = rawData => {
-  rawData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
+  rawData.sort((a, b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
 
   rawData.forEach(articleObject => Article.all.push(new Article(articleObject)))
 }
 
-// REVIEW: This function will retrieve the data from either a local or remote source, and process it, then hand off control to the View.
+// REVIEWED: This function will retrieve the data from either a local or remote source, and process it, then hand off control to the View.
 Article.fetchAll = () => {
   // REVIEW: What is this 'if' statement checking for? Where was the rawData set to local storage?
   if (localStorage.rawData) {
@@ -48,6 +48,16 @@ Article.fetchAll = () => {
     Article.loadAll();
 
   } else {
+    $.getJSON('./data/hackerIpsum.json').then(
+      function(data) {
+        let rawData = data;
+        localStorage.setItem(rawData);
 
+        Article.loadAll();
+      },
+      function(err) {
+        console.error(err);
+      }
+    )
   }
 }
