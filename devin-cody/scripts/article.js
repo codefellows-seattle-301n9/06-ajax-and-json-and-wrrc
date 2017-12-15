a5e7f3a1ec3610810a892f519f9d447fd4d21093
cody <codyjeromegreen@gmail.com>
2017-12-14 20:53:38 -0800

    got everything done but the stretch goal

--- devin-cody/scripts/article.js
+++ devin-cody/scripts/article.js
@@ -12,16 +12,16 @@ function Article (rawDataObj) {
 // REVIEW: Instead of a global `articles = []` array, let's attach this list of all articles directly to the constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves objects, which means we can add properties/values to them at any time. In this case, the array relates to ALL of the Article objects, so it does not belong on the prototype, as that would only be relevant to a single instantiated Article.
 Article.all = [];
 
-// COMMENT: Why isn't this method written as an arrow function?
-// PUT YOUR RESPONSE HERE
+// COMMENTED: Why isn't this method written as an arrow function?
+// because the general rule of thumb of this.
 Article.prototype.toHtml = function() {
   let template = Handlebars.compile($('#article-template').text());
 
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
 
-  // COMMENT: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
+  // COMMENTED: What is going on in the line below? What do the question mark and colon represent? How have we seen this same logic represented previously?
   // Not sure? Check the docs!
-  // PUT YOUR RESPONSE HERE
+  //it is basically an if else statement if this.publishedStatus = publishedOn then write string published $... else it will write out draft
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
   this.body = marked(this.body);
 
@@ -33,7 +33,7 @@ Article.prototype.toHtml = function() {
 // REVIEW: This function will take the rawData, how ever it is provided, and use it to instantiate all the articles. This code is moved from elsewhere, and encapsulated in a simply-named function for clarity.
 
 // COMMENTED: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
-// PUT YOUR RESPONSE HERE
+
 //This function is called in the fetchAll function. rawaData is not a JSON data file vs a JS file with an array of objects.
 Article.loadAll = rawData => {
   rawData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
@@ -45,24 +45,26 @@ Article.loadAll = rawData => {
 Article.fetchAll = () => {
   // REVIEW: What is this 'if' statement checking for? Where was the rawData set to local storage?
   if (localStorage.rawData) {
-    //TODO
-    Article.loadAll();
+    //TODONE
+    let getData = localStorage.getItem('rawData');
+    let blogData = JSON.parse(getData);
     console.log('I am local storage');
+    Article.loadAll(blogData);
+    Article.all.forEach(Article => {
+      $('#articles').append(Article.toHtml())});
 
   } else {
-    //TODO
+    //TODONE
     let rawData = $.getJSON('../data/hackerIpsum.json').then(
       function(rawData) {
         console.log(rawData);
         Article.loadAll(rawData);
         Article.all.forEach(Article => {
           $('#articles').append(Article.toHtml())})
-        articleView.populateFilters();
-        articleView.handleCategoryFilter();
-        articleView.handleAuthorFilter();
-        articleView.handleMainNav();
-        articleView.setTeasers();
+        localStorage.setItem('rawData',JSON.stringify(rawData));
+        console.log('setting local storage');
       },
+      // COMMENT: we determined the sequence for this section by a lot of trial and error. we knew the load.all had to run first to populate the article array and then the forEach method could come in and append the array to the DOM.
       function(err){
         console.error(err);
       }
